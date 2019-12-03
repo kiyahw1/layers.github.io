dba877170fe6830db7b0a62284543e1485d5f019
kiyahw1 <46540335+kiyahw1@users.noreply.github.com>
2019-12-02 23:46:16 -0500

    Updated almost everything

--- _site/flipCard.js
+++ _site/flipCard.js
@@ -6,9 +6,9 @@ function imageChange() {
     let filename = fullname.substring(fullname.lastIndexOf('/'),fullname.length);
     let frontback = fullname.substring(fullname.lastIndexOf("_"),fullname.length);
     if (frontback == "_Front.png") {
-        document.getElementById("subject").src= "../SubjectCards"+filename.replace("_Front.png","_Back.png");
+        document.getElementById("subject").src= "../_assets/SubjectCards/"+filename.replace("_Front.png","_Back.png");
     } else {
-        document.getElementById("subject").src= "../SubjectCards"+filename.replace("_Back.png","_Front.png");
+        document.getElementById("subject").src= "../_assets/SubjectCards/"+filename.replace("_Back.png","_Front.png");
     }
     alert
 }
--- _site/randomSubject.js
+++ _site/randomSubject.js
@@ -8,10 +8,17 @@ function drawSubject(){
     
     randomCard=subjectArray[Math.floor(Math.random()*subjectArray.length)];
 
-    document.getElementById("subject").src= "../SubjectCards/"+randomCard;
+    document.getElementById("subjectCard").src= "../_assets/SubjectCards/"+randomCard;
+
+    //Updates instructions, next button, and draw student button
+    document.getElementById("Body").innerHTML="This is your subject card. If you are not familiar with your topics, flip the card to read some examples. You will have access to this card as you create your activites. ";
+    document.getElementById('subjectNext').style.visibility = 'visible';
+    document.getElementById('arrowFlip').style.visibility='visible';
+    document.getElementById("subjectDraw").value = 'Redraw Subject Cards';
 
     let cookievalue = randomCard + ";";
     document.cookie = "subject=" + cookievalue +"; path/";
     //document.write ("Setting Cookies: " + "entry.1814210675=" + cookievalue);
+    alert(cookievalue)
 }
 
