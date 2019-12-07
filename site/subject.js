9ac0b15dd72e28572a5de26db50506e19c309bbb
kiyahw1 <46540335+kiyahw1@users.noreply.github.com>
2019-12-07 13:13:29 -0500

    Updated Layer 1 Pages

--- site/subject.js
+++ site/subject.js
@@ -1,3 +1,27 @@
+function drawSubject(){
+  let subjectArray = new Array('History_Front.png', 
+  'earthScience_Front.png',
+  'figurativeLanguage_Front.png', 
+  'biology_Front.png', 
+  'Math_Front.png', 
+  'computerScience_Front.png')
+  
+  randomCard=subjectArray[Math.floor(Math.random()*subjectArray.length)];
+
+  document.getElementById("subjectCard").src= "../assets/SubjectCards/"+randomCard;
+
+  //Updates instructions, next button, and draw student button
+  document.getElementById("Body").innerHTML="This is your subject card. If you are not familiar with your topics, flip the card to read some examples. You will have access to this card as you create your activites. ";
+  document.getElementById('subjectNext').style.visibility = 'visible';
+  document.getElementById('arrowFlip').style.visibility='visible';
+  document.getElementById("subjectDraw").value = 'Redraw Subject Cards';
+
+  let cookievalue = randomCard + ";";
+  document.cookie = "subject=" + cookievalue +"; path/";
+  //document.write ("Setting Cookies: " + "entry.1814210675=" + cookievalue);
+  alert(cookievalue)
+}
+
 function getSubject() {
     var name = 'subject' + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
