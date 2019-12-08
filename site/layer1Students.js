b4f544d5a10157ce72c265ad5276a08880777853
kiyahw1 <46540335+kiyahw1@users.noreply.github.com>
2019-12-08 18:36:31 -0500

    Functional Layer 1 Page

--- site/layer1Students.js
+++ site/layer1Students.js
@@ -12,3 +12,34 @@ function getStudent1Layer1(){
     }
     }
 }
+
+
+function getStudent2Layer1(){
+    var name = 'student_2' + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+        document.getElementById('s2l1').src="../assets/StudentCards/" + c.substring(name.length, c.length);
+    }
+    }
+}
+
+function getStudent3Layer1(){
+    var name = 'student_3' + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+        document.getElementById('s3l1').src="../assets/StudentCards/" + c.substring(name.length, c.length);
+    }
+    }
+}
