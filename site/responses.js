2bcb66a7f53a3023cf78004772d923943a459849
kiyahw1 <46540335+kiyahw1@users.noreply.github.com>
2019-12-09 17:12:27 -0500

    Testing response cookie

---
+++ site/responses.js
@@ -0,0 +1,17 @@
+function getResponse() {
+    alert("hi");
+    var name = 'layer_1' + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            document.getElementById('Body').innerHTML= c.substring(name.length, c.length);
+        }
+    }
+    alert("hi");
+    return "";
+  }
