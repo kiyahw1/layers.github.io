8110cc21fa7861d693e775ecd25c60a343b518bd
kiyahw1 <46540335+kiyahw1@users.noreply.github.com>
2020-02-01 14:10:42 -0500

    fixed cookies

--- scripts/Subject.js
+++ scripts/Subject.js
@@ -33,3 +33,21 @@ function imageChange() {
       document.getElementById("subject").src= "../assets/SubjectCards/"+filename.replace("_Back.png","_Front.png");
       frontback = "_Front.png"
   }
+
+
+function getSubjectCard() {
+  var name = 'subject' + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+
+    if (c.indexOf(name) == 0) {
+      document.getElementById('subjectCard').src="../assets/SubjectCards/" + c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
--- scripts/cookies.js
+++ scripts/cookies.js
@@ -26,5 +26,5 @@ class Cookie {
         alert(cookievalue)
         
         }
+    }
 }
-
