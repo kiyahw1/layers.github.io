26122fee52d7fcf6ba17922a266278069e223e54
kiyahw1 <46540335+kiyahw1@users.noreply.github.com>
2020-02-02 14:14:40 -0500

    testing cookies

--- scripts/studentlayers.js
+++ scripts/studentlayers.js
@@ -22,15 +22,4 @@ function changelayer(direction,current){
     alert(currentlayer)
     
 }
-    var name = number + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-        document.getElementById(id).src="../assets/StudentCards/Layer"+currentlayer+'_'+c.substring(name.length, c.length);
-    }
-    }
+
