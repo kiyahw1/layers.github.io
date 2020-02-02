ad610b032116670449fd3c4c4b7e4b6e35548242
kiyahw1 <46540335+kiyahw1@users.noreply.github.com>
2020-02-01 21:10:30 -0500

    testing cookies

---
+++ scripts/navigation.js
@@ -0,0 +1,23 @@
+function navigation(layer){
+    var name = layer + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            alert('pulled');
+            return (c.substring(name.length, c.length)+'.html');
+        }
+    }
+}
+
+function setlayer(layer){;
+    let cookievalue = layer + ";";
+    document.cookie = 'currentlayer' + '=' + cookievalue +"; path/";
+    alert(cookievalue)
+    
+
+}
