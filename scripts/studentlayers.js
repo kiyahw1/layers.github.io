d2c76b289f83a393fd1e1969240a165dc605fb21
kiyahw1 <46540335+kiyahw1@users.noreply.github.com>
2020-02-02 17:34:08 -0500

    testing card flip

--- scripts/studentlayers.js
+++ scripts/studentlayers.js
@@ -13,11 +13,29 @@ function getStudent(number,id){
     }
 }
 
-function changelayer(direction,current){
+function changelayer(direction,current,student,id){
     if (current=='Layer2_Home'){
         if (direction=='down'){
             currentlayer+=1
+            document.getElementById('downarrow').style.display='hidden'
+            document.getElementById('uparrow').style.display='block'
+            }else{
+            currentlayer-=1
+            document.getElementById('uparrow').style.display='hidden'
+            document.getElementById('downarrow').style.display='block'
         }
+    }
+    var name = student + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+        document.getElementById(id).src="../assets/StudentCards/Layer"+toString(currentlayer)+'_'+c.substring(name.length, c.length);
+    }
     }
     alert(currentlayer)
     
