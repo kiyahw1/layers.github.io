8c427a0cf719c8482bdb68bd82c208772664e46b
kiyahw1 <46540335+kiyahw1@users.noreply.github.com>
2020-02-02 15:12:57 -0500

    testing arrows

--- scripts/studentlayers.js
+++ scripts/studentlayers.js
@@ -25,7 +25,7 @@ function changelayer(direction,current){
 
 function displayarrows(current){
     if(current=='Layer2_Home' || current=='Layer3_Home'){
-        document.getElementById('downbutton').style.display='block';
+        document.getElementById('downarrow').style.display='block';
     alert(current)
     }
 }
